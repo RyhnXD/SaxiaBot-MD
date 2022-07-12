f399b03a5517c9e8c45763a9b9d31ce377f8ba46
Mshunone <100399123+Mshunone@users.noreply.github.com>
2022-07-12 07:51:57 +0700

    Update XeonCheems4.js

--- XeonCheems4.js
+++ XeonCheems4.js
@@ -8809,7 +8809,7 @@ XeonBotInc.sendMessage(from, { react: { text: `${global.reactmoji}`, key: m.key 
                             }, {
                                 callButton: {
                                     displayText: 'Sewa',
-                                    id: 'sewa'
+                                     url: `${script}`
                                 }
                             }, {
                                 quickReplyButton: {
